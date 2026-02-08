04365f9ec12ed6444df9c642b7001b86fa4f8be7
Ahmed Frag <a.frag166@gmail.com>
2026-02-08 18:05:54 +0200

    edited paths for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css';
 import data from './data.json';
 
 function App() {
+  console.log('data', data);
+
   return (
     <div className='videos'>
       {data.map((vid, index) => {
         return (
           <li key={index}>
-            <video src={vid} controls muted />
+            <video controls muted>
+              <source src={vid} type='video/mp4' />
+            </video>
           </li>
         );
       })}
