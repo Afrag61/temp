dc2b6cc02f815f964bd86e4eb24a826052aacca5
Ahmed Frag <a.frag166@gmail.com>
2026-02-11 17:37:10 +0200

    new video

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import Header from './components/Header';
-import data from './data.json';
+import Header from "./components/Header";
+import data from "./data.json";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div className='videos'>
-        {data.map((vid, index) => {
-          const name = vid.split('/')[3];
+    return (
+        <>
+            <Header />
+            <div className="videos">
+                {data.map((vid, index) => {
+                    const name = vid.split("/")[3];
 
-          return (
-            <li key={index}>
-              <video
-                controls
-                muted
-                controlsList='nodownload noplaybackrate'
-                disablePictureInPicture
-                onContextMenu={(e) => e.preventDefault()}
-              >
-                <source src={vid} type='video/mp4' />
-              </video>
-              <p>{name}</p>
-            </li>
-          );
-        })}
-      </div>
-    </>
-  );
+                    return (
+                        <li key={index}>
+                            <video
+                                controls
+                                muted
+                                controlsList="nodownload noplaybackrate"
+                                disablePictureInPicture
+                                onContextMenu={e => e.preventDefault()}
+                            >
+                                <source src={vid} type="video/mp4" />
+                            </video>
+                            <p>{name}</p>
+                        </li>
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 export default App;
