7038fd520517aa430c3200c5d6c18360da1115af
Ahmed Frag <a.frag166@gmail.com>
2026-02-14 21:26:52 +0200

    Refactor modal layout to improve structure and enhance video display

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,10 +19,21 @@ const Modal = () => {
       {isOpen && (
         <div className='backdrop z-20 fixed w-full h-full top-0 left-0 bg-black/30 backdrop-blur-md transition-all duration-300 ease-in-out flex items-center justify-center'>
           <div onClick={closeModal} className='w-full h-full absolute' />
-          <div className='modal z-20 w-auto max-w-[90%] md:w-auto md:max-w[90%] h-auto max-h-[90%] md:h-[90%] flex flex-col bg-[#242424] rounded-md justify-around items-center gap-5 p-5'>
+          <div className='modal z-20 w-auto max-w-[90%] md:w-auto md:max-w[90%] h-auto max-h-[90%] md:h-[90%] flex flex-col bg-[#242424] rounded-md justify-around items-center gap-2.5 p-5'>
             {media?.type === 'video' && media.video && (
               <>
-                <h1 className='md:text-lg text-center'>{media.video.name}</h1>
+                <div className='w-full flex flex-row items-center justify-between'>
+                  <h1 className='w-[95%] md:text-nowrap text-center text-pink-300'>
+                    {media.video.name}
+                  </h1>
+
+                  <button
+                    className='justify-self-end cursor-pointer w-8 h-8 text-center bg-pink-300 text-black rounded-lg hover:bg-red-600 transition-all duration-300 ease-in-out font-sans font-bold'
+                    onClick={closeModal}
+                  >
+                    X
+                  </button>
+                </div>
                 <video
                   controls
                   controlsList='nodownload noplaybackrate'
@@ -34,14 +45,6 @@ const Modal = () => {
                 </video>
               </>
             )}
-            <div>
-              <button
-                className='absolute top-3 right-3 cursor-pointer w-10 h-10 text-center bg-[#ff47ac] rounded-full hover:bg-red-600 transition-all duration-300 ease-in-out font-sans font-bold'
-                onClick={closeModal}
-              >
-                X
-              </button>
-            </div>
           </div>
         </div>
       )}
