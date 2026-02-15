06fb62494d2eff94d52cc22571c9ef6b8874f6f6
Ahmed Frag <a.frag166@gmail.com>
2026-02-15 05:09:29 +0200

    Enhance VideoItem component with GSAP animations and refactor for improved structure

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,22 +1,52 @@
+import { useGSAP } from '@gsap/react';
 import { useModalContext } from '../store/useModalCtx';
 import type { TVideo } from '../types';
+import gsap from 'gsap';
+import { useRef } from 'react';
 
 interface Props {
   video: TVideo;
 }
 
 const VideoItem: React.FC<Props> = ({ video }) => {
+  const vidItemRef = useRef<HTMLLIElement>(null);
   const { openModal } = useModalContext();
 
   const handleClick = () => {
     openModal({ type: 'video', video });
   };
 
+  useGSAP(() => {
+    const vid = vidItemRef.current;
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: vidItemRef.current,
+          start: 'top 85%',
+          end: 'bottom 99%',
+          scrub: true,
+        },
+      })
+      .fromTo(
+        vid,
+        { opacity: 0, y: 200, scale: 0.1 },
+        {
+          opacity: 1,
+          y: 0,
+          scale: 1,
+          duration: 0.3,
+          ease: 'back.inOut',
+        },
+      );
+  }, []);
+
   return (
     <li
+      ref={vidItemRef}
       key={video.name}
       onClick={handleClick}
-      className='flex flex-col p-2.5 hover:scale-105 hover:border cursor-pointer border-[#ff47ac] rounded-md transition-all duration-300 ease-in-out object-center'
+      className='item flex flex-col p-2.5 hover:scale-105 hover:border cursor-pointer border-[#ff47ac] rounded-md transition-all duration-300 ease-in-out object-center'
     >
       <div className='aspect-video overflow-hidden rounded-xl bg-black'>
         <img
